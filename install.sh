0fcde3308129fd9c4928aaec3a4030114b4c3670
Austin Conner <aconner.vu@gmail.com>
2013-09-17 06:02:14 -0400

    make zathura keybinds more like less

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-for i in $(ls | grep -v install.sh); do
+for i in $(ls | grep -v 'install.sh\|config'); do
   ln -svTir $i ~/.$i
 done
+cd config
+for i in $(ls); do
+  ln -svTir $i ~/.config/$i
+done
+cd -
