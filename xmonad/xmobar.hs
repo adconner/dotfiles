32717f328b8e3a4a0c59aaffab6a770a4077cb33
Austin Conner <aconner.vu@gmail.com>
2019-04-21 03:04:28 -0500

    configure xmobar

--- xmonad/xmobar.hs
+++ xmonad/xmobar.hs
@@ -1,6 +1,5 @@
 Config { 
-  -- font = "-*-terminus-bold-r-*-*-*-140-*-*-*-*-iso8859-1"
-font = "-misc-fixed-bold-r-bold--18-120-100-100-c-90-iso10646-1"
+         font = "xft:Bitstream Vera Sans Mono:size=12:antialias=true"
        , borderColor = "#242424"
        , border = TopB
        , bgColor = "#242424"
@@ -12,18 +11,18 @@ font = "-misc-fixed-bold-r-bold--18-120-100-100-c-90-iso10646-1"
                                    ["-t","<tempF> F <skyCondition>",
                                     "-L","65","-H","80","--normal","#95e454",
                                     "--high","#ff2026","--low","lightblue"] 36000
-                    , Run Wireless "wlp2s0" ["-t", "<essid> <quality>%"] 300
-                    , Run MultiCpu ["-t", "cpu: <total0>% <total1>% <total2>% <total3>%" ] 100
+                    , Run Wireless "wlp0s20f3" ["-t", "<essid> <quality>%"] 300
+                    , Run MultiCpu ["-t", "cpu: <total0>% <total1>% <total2>% <total3>% <total4>% <total5>% <total6>% <total7>%" ] 100
                     , Run Memory ["-t","mem: <usedratio>%"] 100
-                    , Run Swap ["-t","swap: <usedratio>%"] 100
-                    , Run Battery  ["-t", "<acstatus><timeleft> (<left>%)",
+                    -- , Run Swap ["-t","swap: <usedratio>%"] 100
+                    , Run BatteryP ["CMB0"] ["-t", "<acstatus><timeleft> (<left>%)",
                                     "-L", "10", "-H", "80", "--",
                                     "-O", "<fc=#95e454>AC</fc> - ",
                                     "-o", "",
                                     "-f", "ADP1/online"] 600 
                     , Run Date "%a %b %_d %l:%M" "date" 600
                     -- , Run Date "%a %b %_d %l:%M:%S" "date" 10
-                    , Run MPD ["-t", "<artist> - <title> <statei>"] 100
+                    , Run MPD ["-t", "<artist> - <title> <statei>"] 300
                     -- , Volume "default" "Master"  [] 10
                     --                ["-t", "<status><volume>%", "--",
                     --                 "-O", "", "-O", "<fc=#ff2026>mute</fc> - "] 10
@@ -32,7 +31,8 @@ font = "-misc-fixed-bold-r-bold--18-120-100-100-c-90-iso10646-1"
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = "%battery% | %multicpu% | %memory% %swap% | %wlan0wi% } %StdinReader% { %mpd% | %KBNA% | <fc=#eadead>%date%</fc>"
+       , template = "%battery% | %multicpu% | %memory% | %wlp0s20f3wi% } %StdinReader% { %mpd% | %KBNA% | <fc=#eadead>%date%</fc>"
+       -- , template = "%battery% | %multicpu% | %memory% %swap% | %wlan0wi% } %StdinReader% { %mpd% | %KBNA% | <fc=#eadead>%date%</fc>"
        -- , template = "%battery% | %multicpu% | %memory% | %wlan0wi% } %StdinReader% { %mpd% | %default:Master% | %KBNA% | <fc=#eadead>%date%</fc>"
        -- , template = "%battery% | %multicpu% | %memory% | %wlan0wi% } %StdinReader% { %KBNA% | <fc=#eadead>%date%</fc>"
        }
