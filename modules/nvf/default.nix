e8558f4f08b455c7fcf8f401ef2bcad554d74f52
Austin Conner <aconner.vu@gmail.com>
2026-01-02 23:47:32 -0500

    add default command and system

--- modules/nvf/default.nix
+++ modules/nvf/default.nix
@@ -1,5 +1,6 @@
 {inputs, ...}: {
-  perSystem = systemInputs@{pkgs, ... }: {
+  perSystem = systemInputs@{pkgs, self', ... }: {
+    packages.default = self'.packages.neovim;
     packages.neovim = (inputs.nvf.lib.neovimConfiguration {
       inherit pkgs;
       modules = [ (import ./config.nix systemInputs) ];
--- modules/oscar/default.nix
+++ modules/oscar/default.nix
@@ -1,4 +1,5 @@
-{ inputs, ... }: {
+{ inputs, self', ... }: {
+  flake.nixosConfigurations.default = self'.flake.nixosConfigurations.oscar;
   flake.nixosConfigurations.oscar = inputs.nixpkgs.lib.nixosSystem {
     specialArgs = { inherit inputs; };
     modules = [ ./configuration.nix ];
